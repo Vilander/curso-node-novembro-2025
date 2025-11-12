2a6e30b8a56ba2c6296ae43cf5352cc7de4d290f
Vilander Costa <110786239+Vilander@users.noreply.github.com>
2025-11-11 22:27:04 -0300

    feat: get e post

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("Olá mundo")
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const herois = [
+  "Mulher Maravilha",
+  "Superman",
+  "Batman",
+  "Homem Aranha",
+  "Capitã Marvel",
+  "Homem de Ferro",
+];
+
+app.get("/herois", function (req, res) {
+  res.send(herois);
+});
+
+app.get("/herois/:id", function (req, res) {
+  const id = req.params.id;
+  res.send(herois[id - 1]);
+});
+
+app.post("/herois", function (req, res) {
+  let novoHeroi = req.body.nome;
+  herois.push(novoHeroi);
+  res.send("ok");
+});
+
+// app.get("/marvel", function (req, res) {
+//   const heroisMarvel = [];
+//                          0               1                 2
+//   res.send(heroisMarvel);
+// });
+
+app.listen(3000);
